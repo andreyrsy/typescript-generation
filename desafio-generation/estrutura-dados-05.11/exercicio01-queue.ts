5e8dc2fdf413b8fdb82f1da3a3e800812cbfc751
andreyrsy <contatomainort@gmail.com>
2025-11-05 15:42:53 -0300

    Collection Queue[Desafio.05.11]

---
+++ desafio-generation/estrutura-dados-05.11/exercicio01-queue.ts
@@ -0,0 +1,38 @@
+import { Queue } from "../../estrutura-dados/Queue";
+import readlinesync = require("readline-sync")
+
+const fila = new Queue<string>();
+
+ let option: number;
+
+do{
+  console.log("1 - Adicionar Cliente na fila");
+  console.log("2 - Listar todos os Clientes");
+  console.log("3 - Retirar Cliente da Fila");
+  console.log("0 - Sair");
+  console.log("--------------------------------------------")
+  option = readlinesync.questionInt("Entre com a opcao desejada: ");
+
+  switch(option){
+    case 1:
+      let cliente = readlinesync.question("\nNome do cliente: ")
+      fila.enqueue(cliente);
+      fila.printQueue();
+      console.log("\nCliente adicionado!\n")
+      break;
+    case 2:
+      console.log("\nLista de clientes na fila: ")
+      fila.printQueue();
+      console.log("--------------------------------------------");
+      break;
+    case 3:
+      fila.dequeue();
+      fila.printQueue();
+      console.log("--------------------------------------------\n");
+      break;
+    default:
+      console.log("Saindo do programa...")
+      break;
+  }
+
+}while(option != 0);
