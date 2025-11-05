b6b53e25f89b1070a93eec738170ccdda84630ce
andreyrsy <contatomainort@gmail.com>
2025-11-05 11:39:34 -0300

    Estrutura-Dados[05.11]

---
+++ estrutura-dados/TestaStack.ts
@@ -0,0 +1,30 @@
+import { Stack } from "./Stack";
+
+const pilha = new Stack<string>();
+
+pilha.push("Prato 1");
+pilha.push("Prato 2");
+pilha.push("Prato 3");
+pilha.push("Prato 4");
+
+console.log("Pratos na pilha");
+
+pilha.printStack();
+
+console.log(`Prato removido: ${pilha.pop()}`)
+
+pilha.printStack();
+
+pilha.push("Prato 5");
+
+console.log(`Prato do topo: ${pilha.peek()}`);
+
+pilha.printStack();
+
+console.log(`Tamanho da pilha: ${pilha.count()}`);
+
+console.log(`O prato 3 existe na pilha? ${pilha.contains("Prato 3")}`);
+
+pilha.clear();
+
+console.log(`A pilha esta vazia? ${pilha.isEmpty()}`);
